1a6f2fe915782f89a37e5203f349123e3c60f1eb
4books <joonwoo9192@gmail.com>
2022-06-20 00:05:48 +0900

    JdbcTemplate and Test

--- src/main/java/hello/itemservice/config/JdbcTemplateV1Config.java
+++ src/main/java/hello/itemservice/config/JdbcTemplateV1Config.java
@@ -1,14 +1,22 @@
 package hello.itemservice.config;
 
 import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jdbctemplate.JdbcTemplateItemRepositoryV1;
+import hello.itemservice.repository.jdbctemplate.JdbcTemplateItemRepositoryV2;
 import hello.itemservice.repository.memory.MemoryItemRepository;
 import hello.itemservice.service.ItemService;
 import hello.itemservice.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
-public class MemoryConfig {
+@RequiredArgsConstructor
+public class JdbcTemplateV1Config {
+
+    private final DataSource dataSource;
 
     @Bean
     public ItemService itemService() {
@@ -17,6 +25,6 @@ public class MemoryConfig {
 
     @Bean
     public ItemRepository itemRepository() {
-        return new MemoryItemRepository();
+        return new JdbcTemplateItemRepositoryV1(dataSource);
     }
 }
